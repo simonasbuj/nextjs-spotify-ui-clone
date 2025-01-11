49817df922603f99a685e15f251debfcd6a10728
unknown <simonas.b@hotmail.com>
2025-01-11 23:07:41 +0200

    small naming fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Ubuntu, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 
-
 const ubuntu = Ubuntu({
   subsets: ["latin"],
   variable: "--font-ubuntu",
