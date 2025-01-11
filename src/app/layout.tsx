e1cb90b0e6f77d58449e53be40ea117edd919a68
unknown <simonas.b@hotmail.com>
2025-01-11 22:58:58 +0200

    Finished header for now with some very basic functions on searchbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu, Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/layout/header";
+import Header from "@/components/layout/Header";
 
 
 const ubuntu = Ubuntu({
---
+++ src/components/ui/VerticalDivider.tsx
@@ -0,0 +1,5 @@
+export default function VerticalDivider() {
+    return (
+        <div className="w-px h-auto bg-gray-500 mx-2 self-stretch mx-"></div>
+    );
+}
